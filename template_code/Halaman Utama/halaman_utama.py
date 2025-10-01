cb997aee246420caba4546ba04eb435d041a983f
daffascript <daffa.124450061@student.itera.ac.id>
2025-10-01 22:51:24 +0700

    Test

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,19 +102,14 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;"> kelompok poisson adalahs</div>""",
+            """<div style="text-align: justify;"> kelompok poisson adalah</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
         foto_kelompok = "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_"
         layout(foto_kelompok)
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Kelompok 02 - Poisson.</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
