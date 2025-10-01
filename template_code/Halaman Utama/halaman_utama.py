6aa8d632e0eaf472e697ef16e8412e51ea1a2b09
vannisaramadhani <vnnsrmdhn@gmail.com>
2025-10-01 23:33:21 +0700

    deskripsi kelompok markov!

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,12 +102,9 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Markov adalah nama kelompok yang diambil dari istilah dalam matematika yaitu proses Markov, yang berarti sebuah sistem yang bergerak maju berdasarkan kondisi saat ini tanpa bergantung pada masa lalu. 
+                    Nama ini dipilih karena melambangkan semangat untuk selalu fokus pada langkah yang ada di depan, serta menjadikan setiap pengalaman sebagai pijakan untuk berkembang.
+                    Kelompok Markov beranggotakan 12 orang yang masing-masing memiliki peran penting dalam menjaga kekompakan. Seperti proses Markov yang terdiri dari keadaan-keadaan yang saling berhubungan, setiap anggota kelompok saling melengkapi dan membentuk alur kebersamaan.</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
