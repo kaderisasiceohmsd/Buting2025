b359d62fb3cac86f41b15bb532dc9fb6413dee9c
Muhammad Razan <muhammad.124450031@student.itera.ac.id>
2025-10-01 20:31:06 +0700

    Penulisan Deskripsi kelompok

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,12 +102,12 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    uis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est 
-                    laborum.</div>""",
+            """<div style="text-align: justify;">Kelompok 1 Tensor bukan hanya sekelompok nama; kami adalah simpul keakraban dan keharmonisan. 
+            Nama "Tensor" sendiri melambangkan kekuatan yang menyatukan berbagai elemen, dan itulah yang kami lakukan. Di sini, setiap individu dihargai, 
+            dan suasana yang tercipta terasa hangat—seperti keluarga kedua tempat setiap anggota bebas bercerita, tertawa, dan tumbuh bersama tanpa rasa 
+            canggung. Kami percaya bahwa kaderisasi terbaik lahir dari kebersamaan yang solid. Kami belajar, berdiskusi, dan berproses dengan semangat 
+            saling dukung dan gotong royong. Kelompok Tensor adalah bukti nyata bahwa ketika kita selaras, proses belajar pun terasa ringan dan 
+            penuh makna.</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
