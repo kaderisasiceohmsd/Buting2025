4f2707a7afb76e459ec85d89b0aa38f94e367fef
unknown <afriza.azmi2006@gmail.com>
2025-10-05 19:42:58 +0700

    pliss

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,7 +102,7 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;"> Ini adalah kelpompok Anova , paling gacor</div>""",
+            """<div style="text-align: justify;"> Ini adalah kelpompok Anova , paling gacorrr</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
