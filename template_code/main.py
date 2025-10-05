e0b45fdcd07777309c4d47cdfa07f61e8d27a43d
bimayasa <bima.124450106@student.itera.ac.id>
2025-10-05 14:25:55 +0700

    update folder anggota

--- template_code/main.py
+++ template_code/main.py
@@ -9,8 +9,68 @@ Homepage = st.Page("Halaman Utama/halaman_utama.py",
     default=True)
 
 Mahasiswa1 = st.Page(
-    "Buku Kating/117_Nobel Nizam Fathirizki.py",
-    title="117 - Nobel Nizam Fathirizki",
+    "Buku Kating/106_Bima Ekayasa.py",
+    title="106 - Bima Ekayasa",
+    icon=":material/person:",
+)
+Mahasiswa2 = st.Page(
+    "Buku Kating/014_Aqila Zayyan Salsabil.py",
+    title="014 - Aqila Zayyan Salsabil",
+    icon=":material/person:",
+)
+Mahasiswa3 = st.Page(
+    "Buku Kating/126_Faizatun Najmi.py",
+    title="126 - Faizatun Najmi",
+    icon=":material/person:",
+)
+Mahasiswa4 = st.Page(
+    "Buku Kating/039_Manuel Frederika.py",
+    title="039 - Manuel Frederika",
+    icon=":material/person:",
+)
+Mahasiswa5 = st.Page(
+    "Buku Kating/027_Sherena Florencia.py",
+    title="027 - Sherena Florencia",
+    icon=":material/person:",
+)
+Mahasiswa6 = st.Page(
+    "Buku Kating/098_Edsel Adya Pradipta.py",
+    title="098 - Edsel Adya Pradipta",
+    icon=":material/person:",
+)
+Mahasiswa7 = st.Page(
+    "Buku Kating/128_Muhammad Harvinsyah.py",
+    title="128 - Muhammad Harvinsyah",
+    icon=":material/person:",
+)
+Mahasiswa8 = st.Page(
+    "Buku Kating/113_Ahmad Bimo Akbar Arkana Putra.py",
+    title="113 - Ahmad Bimo Akbar Arkana Putra",
+    icon=":material/person:",
+)
+Mahasiswa9 = st.Page(
+    "Buku Kating/080_Abigael Limbong.py",
+    title="080 - Abigael Limbong",
+    icon=":material/person:",
+)
+Mahasiswa10 = st.Page(
+    "Buku Kating/075_Faiza Try Anjani.py",
+    title="075 - Faiza Try Anjani",
+    icon=":material/person:",
+)
+Mahasiswa11 = st.Page(
+    "Buku Kating/074_Saskia Nova Magdalena.py",
+    title="074 - Saskia Nova Magdalena",
+    icon=":material/person:",
+)
+Mahasiswa12 = st.Page(
+    "Buku Kating/061_Daffa Kharisma Adzana.py",
+    title="061 - Daffa Kharisma Adzana",
+    icon=":material/person:",
+)
+Mahasiswa13 = st.Page(
+    "Buku Kating/035_Tubagus Abdani Pamungkas.py",
+    title="035 - Tubagus Abdani Pamungkas",
     icon=":material/person:",
 )
 
@@ -23,7 +83,11 @@ if st.session_state.pindah:
     pg = st.navigation(
         {
             "Halaman Utama": [Homepage],
-            "Buku Kating": [Mahasiswa1],
+            "Buku Kating": [
+                Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5,
+                Mahasiswa6, Mahasiswa7, Mahasiswa8, Mahasiswa9, Mahasiswa10,
+                Mahasiswa11, Mahasiswa12, Mahasiswa13
+            ],
             "Try Me !!": [KREASI, KREASII],
         }
     )
