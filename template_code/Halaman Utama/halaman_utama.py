a05f00844e77fa0caaf03c46c58ca18f5bc1dd1f
jeremiahalim <jeremia.124450101@student.itera.ac.id>
2025-10-07 11:54:35 +0700

    Deksripsi Anova!

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,7 +102,7 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Testing Butu Kating Anovaaa!!!</div>""",
+            """<div style="text-align: justify;">Testing Butu Kating Anovaaaa!!!</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
