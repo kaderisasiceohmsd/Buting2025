a7bd107b99be1b63434b759329b4fc402d289db4
jeremiahalim <jeremia.124450101@student.itera.ac.id>
2025-10-07 12:50:49 +0700

    Cihuy Anova

--- template_code/main.py
+++ template_code/main.py
@@ -11,6 +11,53 @@ Homepage = st.Page("Halaman Utama/halaman_utama.py",
 Mahasiswa1 = st.Page(
     "Buku Kating/117_Nobel Nizam Fathirizki.py",
     title="117 - Nobel Nizam Fathirizki",
+    "Buku Kating/101_Jeremia Halim.py",
+    title="101 - Jeremia Halim",
+    icon=":material/person:",
+)
+Mahasiswa2 = st.Page(
+    "Buku Kating/010_Asri Meilani.py",
+    title="010 - Asri Meilani",
+    icon=":material/person:",
+)
+Mahasiswa3 = st.Page(
+    "Buku Kating/043_Difanya Husakina.py",
+    title="043 - Difanya Husakina",
+    icon=":material/person:",
+)
+Mahasiswa4 = st.Page(
+    "Buku Kating/049_Cerine Sihotang.py",
+    title="049 - Cerine Sihotang",
+    icon=":material/person:",
+)
+Mahasiswa5 = st.Page(
+    "Buku Kating/050_Anash Tasya Ausyaqila",
+    title="050 - Anash Tasya Ausyaqila",
+    icon=":material/person:",
+)
+Mahasiswa6 = st.Page(
+    "Buku Kating/070_Fitra Pratama Mega.py",
+    title="070 - Fitra Pratama Mega",
+    icon=":material/person:",
+)
+Mahasiswa7 = st.Page(
+    "Buku Kating/110_Afriza Azmi.py",
+    title="110 - Afriza Azmi",
+    icon=":material/person:",
+)
+Mahasiswa8 = st.Page(
+    "Buku Kating/121_Jona Timothy Ogatse Panjaitan.py",
+    title="121 - Jona Timothy Ogatse Panjaitan",
+    icon=":material/person:",
+)
+Mahasiswa9 = st.Page(
+    "Buku Kating/124_Shafa Deilaila Azzahra.py",
+    title="124 - Shafa Delaila Azzahra",
+    icon=":material/person:",
+)
+Mahasiswa10 = st.Page(
+    "Buku Kating/130_Ahmad Zidane Rabbaanee.py",
+    title="130 - Ahmad Zidane Rabbaanee",
     icon=":material/person:",
 )
 
