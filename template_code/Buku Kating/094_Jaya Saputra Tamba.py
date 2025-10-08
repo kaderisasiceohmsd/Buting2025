ac7ab1bce4682bdfc545992fd5881ab362c3553b
ferazka <ferazkaa@gmail.com>
2025-10-08 11:51:27 +0700

    tes

--- template_code/Buku Kating/094_Jaya Saputra Tamba.py
+++ template_code/Buku Kating/094_Jaya Saputra Tamba.py
@@ -105,7 +105,7 @@ if menu == "Kesekjenan":
         ]
         data_list = [
             {
-                "nama": "nasi goreng 1",
+                "nama": "nasi goreng 1, ga pake timun",
                 "nim": "122450000",
                 "umur": "18",
                 "asal":"Bekasi",
