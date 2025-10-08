7e6200325495267e0d67c1e07fd49851b9f4de00
riska erlis dayu  tiara <riska.124450022@student.itera.ac.id>
2025-10-08 22:17:19 +0700

    ubah biodata riska

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -198,6 +198,13 @@ elif menu == "About Us":
                 "fun_fact": "nyemilin es bata, kiko",
                 "motto_hidup": "jalanin dulu keles",
             },
+            {
+                "nama": "Riska Erlis Dayu Tiara",
+                "sebagai": "Anggotaa",
+                "nim": "12450022",
+                "fun_fact": "suka bengong liat sekitar",
+                "motto_hidup": "setiap tantangan adalah peluang untuk berkembang",
+            },
         ]
         display_images_with_data(gambar_urls, data_list)
 
