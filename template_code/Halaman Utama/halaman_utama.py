5aa01151c7ae08e663332d62781ed260d2343282
KalebIstel <kaleb.124450053@student.itera.ac.id>
2025-10-10 11:45:10 +0700

    foto kelompok

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -106,7 +106,7 @@ if menu == "Home":
             unsafe_allow_html=True,
         )
         st.write(""" """)
-        foto_kelompok = "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_"
+        foto_kelompok = "https://drive.google.com/uc?export=view&id=1ERUfITTPfR7SWanlCSTD_Q7LVF9EbRwn"
         layout(foto_kelompok)
         st.markdown(
             """<div style="text-align: justify;">hai ini kaleb.</div>""",
