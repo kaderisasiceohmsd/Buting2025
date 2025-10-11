b5db1a292140792c80f30493e33711bdf8509bdb
Nazlah Auliya <nazlahaulia84@gmail.com>
2025-10-11 21:30:10 +0700

    foto kesekjenan dan baleg

--- template_code/Buku Kating/054_Nazlah Auliya.py
+++ template_code/Buku Kating/054_Nazlah Auliya.py
@@ -181,10 +181,10 @@ if menu == "Kesekjenan":
 if menu == "Baleg":
     def Baleg():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1iXZtlXcpxPPuIdZrrDURGYxDqFoDsQI4",
+            "https://drive.google.com/uc?export=view&id=1iXZtlXcpxPPuIdZrrDURGYxDqFoDsQI4",
+            "https://drive.google.com/uc?export=view&id=1WcxayDm0bLfyvi508mf03ZMBMgPFO0a9",
+            "https://drive.google.com/uc?export=view&id=1mW7LB4kHFBHdILqasLS4gThDw9WIk4Lu",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
