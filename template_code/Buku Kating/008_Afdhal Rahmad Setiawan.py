a49eb3dbb1c7fd49ce1f739ac7ce1c728c2a506e
Afdhal Rahmad Setiawan <adolcahyo7@gmail.com>
2025-10-11 14:43:28 +0700

    Update 008_Afdhal Rahmad Setiawan.py

--- template_code/Buku Kating/008_Afdhal Rahmad Setiawan.py
+++ template_code/Buku Kating/008_Afdhal Rahmad Setiawan.py
@@ -99,12 +99,12 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1R37s_dBPULKh-0ft8ddx0_uUexcOapA",
+            "https://drive.google.com/uc?export=view&id=13IRm7cHxfSlG_Juj8n8ipUgAQ1t9jk9f",
+            "https://drive.google.com/uc?export=view&id=1iKVg4jURomRaACebFQcw9GM9b_cxuAva",
+            "https://drive.google.com/uc?export=view&id=1w93ATzpvcge0Fhqf76AegcEfFWOLIIAO",
+            "https://drive.google.com/uc?export=view&id=11dN7zmgY1MteXQfCSSCuD4cJe0qoImuS",
+            "https://drive.google.com/uc?export=view&id=1tarI5ai3B8LYu2JcejzlzQLgnwmaPbYo",
         ]
         data_list = [
             {
