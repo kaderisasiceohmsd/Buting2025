be4c3db9041f0b780e1979d3851bb64f6b041590
MochIqbalAzzahir <moch.124450052@student.itera.ac.id>
2025-10-11 14:11:38 +0700

    Revert "Merge pull request #39 from kaderisasiceohmsd/iqbal"
    This reverts commit e125922802df9846b4b78270fd1568072034f8cc, reversing
    changes made to bba9394b3bd9b73e52b8f2d56bce20f568010251.

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,11 +102,6 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">Kelompok Bayesian merupakan dua belas bayi naga kecil yang sempat kehilangan arah dalam perjalanan mereka. Di tengah kebingungan itu, 
-            mereka akhirnya bertemu dengan sosok ibu yang penuh kasih bernama Dea Amanda yang dengan sabar sekaligus tegas membimbing mereka.
-            Dari kedua belas naga tersebut ada Nando sebagai naga tertua yang bijak bersama Azzelya yang selalu setia mendampinginya dalam menjaga
-            dan mengarahkan adik-adiknya. Bersama-sama mereka menuntun sepuluh naga lainnya yaitu Yazid, Iqbal, Alsi, Hafis, Zaldi, Bina, Yolanda, Iffa, Arfai dan Yulia agar tidak lagi tersesat. 
-            Dari hari ke hari berkat bimbingan Dea Amanda keluarga naga kecil ini tumbuh semakin kuat kompak dan siap menorehkan jejak besar sebagai satu kesatuan keluarga naga Bayesian.</div>""",
             """<div style="text-align: justify;">
             Teorema Bayes mengajarkan bahwa keyakinan awal (prior) bukanlah sesuatu yang mutlak. Setiap 
             bukti baru (evidence) yang datang dapat memperkaya dan bahkan mengubah keyakinan itu, sehingga lahirlah 
@@ -131,12 +126,6 @@ if menu == "Home":
         foto_kelompok = "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_"
         layout(foto_kelompok)
         st.markdown(
-            """<div style="text-align: justify;">Sebagai simbol, gaya Bayes digambarkan dengan pose unik: 
-            tangan kiri mengepal lurus ke depan, sementara tangan kanan siaga layaknya gerakan bela diri. 
-            Gerakan ini bukan sekadar gaya, tapi punya makna. Tangan kiri yang tegak lurus melambangkan keyakinan awal yang kuat, 
-            sedangkan tangan kanan yang siap menjaga menggambarkan bukti baru yang datang untuk memperkuat arah yang dipilih. 
-            Dengan filosofi itu, naga Bayesian bukan hanya sebuah kelompok, melainkan keluarga yang terus belajar, berkembang,
-            dan semakin kokoh lewat proses pembaruan keyakinan, persis seperti semangat teori Bayes.</div>""",
             """<div style="text-align: justify;">Jadi secara singkat:
             Teorema Bayes memberi filosofi bahwa hidup adalah proses memperbarui keyakinan dan keputusan 
             berdasarkan bukti baru, sehingga kita menjadi lebih adaptif, bijak, dan terus berkembang.</div>""",
@@ -177,24 +166,23 @@ elif menu == "About Us":
                 "motto_hidup": "Do it for the plot",
             },
             {
-
-                "nama": "Moch. Iqbal Az-Zahir",
+                "nama": "Alsi",
                 "sebagai": "Anggota",
-                "nim": "124450052",
-                "fun_fact": "Kadang yapping",
-                "motto_hidup": "Terpuji dalam akhlak, beruntung dalam langkah, nyata dalam jejak.",
+                "nim": "122450083",
+                "fun_fact": "nyemilin es bata",
+                "motto_hidup": "mantap",
             },
             {
-                "nama": "Hafidz Wahdiansyah",
-                "sebagai": "Anggota",
-                "nim": "124450064",
+                "nama": "Bina",
+                "sebagai": "Anggotaa",
+                "nim": "122450000",
                 "fun_fact": "nyemilin",
                 "motto_hidup": "jalanin dulu aja",
             },
             {
-                "nama": "M Afai",
+                "nama": "Hafidz",
                 "sebagai": "Anggotaa",
-                "nim": "124450004",
+                "nim": "122450100",
                 "fun_fact": "nyemilin es bata, kiko",
                 "motto_hidup": "jalanin dulu keles",
             },
@@ -208,7 +196,7 @@ elif menu == "About Us":
             {
                 "nama": "M. Alsi Syahrulloh",
                 "sebagai": "Anggotaa",
-                "nim": "12245082",
+                "nim": "122450100",
                 "fun_fact": "nyemilin es bata, kiko",
                 "motto_hidup": "jalanin dulu keles",
             },
