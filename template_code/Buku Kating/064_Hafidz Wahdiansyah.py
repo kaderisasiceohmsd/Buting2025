3e1bb38aca05b3f98c0160ac717f403691c80b5d
Hafidz Wahdiansyah <167665747+hafidzwahdiansyah@users.noreply.github.com>
2025-10-11 11:58:57 +0700

    Update 064_Hafidz Wahdiansyah.py

--- template_code/Buku Kating/064_Hafidz Wahdiansyah.py
+++ template_code/Buku Kating/064_Hafidz Wahdiansyah.py
@@ -99,7 +99,7 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1U5W5Bwj00-Syg5MJy1WEmOi1jj8Ngfe4",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
         ]
