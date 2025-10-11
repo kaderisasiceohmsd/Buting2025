f4f1c26bb7286adc3974843e98aa487f0a005590
jeremiahalim <jeremia.124450101@student.itera.ac.id>
2025-10-12 02:31:03 +0700

    NYOI

--- template_code/Buku Kating/101_Jeremia Halim.py
+++ template_code/Buku Kating/101_Jeremia Halim.py
@@ -1050,7 +1050,7 @@ if menu == "Departemen Eksternal":
             "https://drive.google.com/uc?export=view&id=1qJNaXGqqfFAhSfwyzT87SGf4lVrDJCu-",
             "https://drive.google.com/uc?export=view&id=12KwhTIZDTrH0ah2lxEouaCH4EPudSBlb",
             "https://drive.google.com/uc?export=view&id=12hS0pHq3TOOi_WGIiuKcf1ZE7zxH05Sw",
-            "https://drive.google.com/uc?export=view&id=1Jw4WI6feZFGS5yjQFHzDIAnvn5fUfmXS",
+            "https://drive.google.com/uc?export=view&id=1-2E5OGLPv42nS-myHmFGWJVKL22KhTRC",
             "https://drive.google.com/uc?export=view&id=15t27MTdJpyvzQh4BQVCJhzxlyFUmFAlW",
             "https://drive.google.com/uc?export=view&id=1pjshzLASeGUGnK9LikNo7t4dc55Ff2E6",
             "https://drive.google.com/uc?export=view&id=19KoXVWMtTheHSJGY9kA71Vzo1tzo9Bcj",
