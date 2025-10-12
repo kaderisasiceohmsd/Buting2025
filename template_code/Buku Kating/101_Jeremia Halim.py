6bb7d32d3717684392ef5153d512b577f056e7f9
afrizaazmi <afriza.azmi2006@gmail.com>
2025-10-12 10:22:18 +0700

    Update 101_Jeremia Halim.py

--- template_code/Buku Kating/101_Jeremia Halim.py
+++ template_code/Buku Kating/101_Jeremia Halim.py
@@ -1050,7 +1050,7 @@ if menu == "Departemen Eksternal":
             "https://drive.google.com/uc?export=view&id=1qJNaXGqqfFAhSfwyzT87SGf4lVrDJCu-",
             "https://drive.google.com/uc?export=view&id=12KwhTIZDTrH0ah2lxEouaCH4EPudSBlb",
             "https://drive.google.com/uc?export=view&id=12hS0pHq3TOOi_WGIiuKcf1ZE7zxH05Sw",
-            "https://drive.usercontent.google.com/download?id=1JcHorGVJvJFcWaPmcKos3HYWd4fpSy4h&export=view&authuser=0",
+            "https://drive.google.com/uc?export=view&id=1Hi3DYrzSoA79xXnf6i7dlM8Xr-wymRLG",
             "https://drive.google.com/uc?export=view&id=15t27MTdJpyvzQh4BQVCJhzxlyFUmFAlW",
             "https://drive.google.com/uc?export=view&id=1pjshzLASeGUGnK9LikNo7t4dc55Ff2E6",
             "https://drive.google.com/uc?export=view&id=19KoXVWMtTheHSJGY9kA71Vzo1tzo9Bcj",
