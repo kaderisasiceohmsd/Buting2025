a10d7cf17ffc2fbaa74c3708a9d2bbf25ca8effe
indahjuliamawarpratiwi <indahjulia05@gmail.com>
2025-10-12 21:09:52 +0700

    deskripsi kelompok

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -102,7 +102,7 @@ if menu == "Home":
             "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
         )
         st.markdown(
-            """<div style="text-align: justify;">test salsabila hahaha</div>""",
+            """<div style="text-align: justify;">GREEDY</div>""",
             unsafe_allow_html=True,
         )
         st.write(""" """)
