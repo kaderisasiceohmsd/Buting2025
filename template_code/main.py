19c5254666dacf6bad11119859401ace5e2445e5
jayasaputratamba <jayatamba17@gmail.com>
2025-10-12 22:30:47 +0700

    Update main.py

--- template_code/main.py
+++ template_code/main.py
@@ -64,7 +64,7 @@ if st.session_state.pindah:
     pg = st.navigation(
         {
             "Halaman Utama": [Homepage],
-            "Buku Kating": [Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5, Mahasiswa6, Mahasiswa7],
+            "Buku Kating": [Mahasiswa1, Mahasiswa2, Mahasiswa3, Mahasiswa4, Mahasiswa5, Mahasiswa6, Mahasiswa7, Mahasiswa8, Mahasiswa9],
             "Try Me !!": [KREASI, KREASII],
         }
     )
