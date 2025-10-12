ee4affd473a924c40c64176dea3051f0d3961bb6
queentathifaalnabila <nabilaqueenta@gmail.com>
2025-10-12 17:05:27 +0700

    lengkpi sekjen queenta

---
+++ template_code/Buku Kating/59_Queenta Thifaal Nabila.py
@@ -0,0 +1,181 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import requests
+from PIL import Image, ImageOps
+from io import BytesIO
+
+st.markdown("""<style>.centered-title {text-align: center;}</style>""",unsafe_allow_html=True)
+st.markdown("<h1 class='centered-title'>BUKU KATING</h1>", unsafe_allow_html=True)
+
+# bagian sini jangan diubah
+def streamlit_menu():
+    selected = option_menu(
+        menu_title=None,
+        options=[
+            "Kesekjenan",
+            "Baleg",
+            "Senator",
+            "Departemen PSDA",
+            "Departemen MIKFES",
+            "Departemen Eksternal",
+            "Departemen Internal",
+            "Departemen SSD",
+            "Departemen Medkraf",
+        ],
+        icons=[
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+            "people-fill",
+        ],
+        default_index=0,
+        orientation="horizontal",
+        styles={
+            "container": {"padding": "0!important", "background-color": "#fafafa"},
+            "icon": {"color": "black", "font-size": "19px"},
+            "nav-link": {
+                "font-size": "15px",
+                "text-align": "left",
+                "margin": "0px",
+                "--hover-color": "#eee",
+            },
+            "nav-link-selected": {"background-color": "#3FBAD8"},
+        },
+    )
+    return selected
+
+@st.cache_data
+def load_image(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        st.error(
+            f"Failed to fetch image from {url}, status code: {response.status_code}"
+        )
+        return None
+    try:
+        img = Image.open(BytesIO(response.content))
+        img = ImageOps.exif_transpose(img)
+        img = img.resize((300, 400))
+        return img
+    except Exception as e:
+        st.error(f"Error loading image: {e}")
+        return None
+    
+@st.cache_data
+def display_images_with_data(gambar_urls, data_list):
+    images = []
+    for i, url in enumerate(gambar_urls):
+        with st.spinner(f"Memuat gambar {i + 1} dari {len(gambar_urls)}"):
+            img = load_image(url)
+            if img is not None:
+                images.append(img)
+
+    for i, img in enumerate(images):
+        # Menggunakan Streamlit untuk menampilkan gambar di tengah kolom
+        col1, col2, col3 = st.columns([1, 2, 1])
+        with col2:
+            st.image(img, use_container_width=True)
+
+        if i < len(data_list):
+            st.write(f"Nama: {data_list[i]['nama']}")
+            st.write(f"NIM: {data_list[i]['nim']}")
+            st.write(f"Umur: {data_list[i]['umur']}")
+            st.write(f"Asal: {data_list[i]['asal']}")
+            st.write(f"Alamat: {data_list[i]['alamat']}")
+            st.write(f"Hobbi: {data_list[i]['hobbi']}")
+            st.write(f"Sosial Media: {data_list[i]['sosmed']}")
+            st.write(f"Kesan: {data_list[i]['kesan']}")
+            st.write(f"Pesan: {data_list[i]['pesan']}")
+            st.write("  ")
+    st.write("Semua gambar telah dimuat!")
+menu = streamlit_menu()
+
+# BAGIAN SINI YANG HANYA BOLEH DIUABAH
+if menu == "Kesekjenan":
+    def kesekjenan():
+        gambar_urls = [
+            "https://drive.google.com/file/d/1LnYMTAAim4mor_uniUbgs8mpbOvTTtRp/view?usp=drivesdk",
+            "https://drive.google.com/file/d/15WhedyljD7bQvJIBpuOQE49gmOV5w71_/view?usp=drivesdk",
+            "https://drive.google.com/file/d/1WPESi1JbEv9E3A6xsNhcCzO2lJ1ao8lx/view?usp=drivesdk",
+            "https://drive.google.com/file/d/1x8y1pqXyiVOxGs4OF3oSTkR_lCkXTmhy/view?usp=drivesdk",
+            "https://drive.google.com/file/d/1kGiOFOpZ-yweciuBowB3wCiiSEleFbHG/view?usp=drivesdk",
+            "https://drive.google.com/file/d/1dKaCqEDIWMqnhMSG3pZS3t4i3Yt_a1c4/view?usp=drivesdk",
+        ]
+        data_list = [
+            {
+                "nama": "Rendra Eka Prayoga",
+                "nim": "122450112",
+                "umur": "21",
+                "asal":"Bekasi",
+                "alamat": "Pulau Damar",
+                "hobbi": "beli donat kentang",
+                "sosmed": "@_erendraa",
+                "kesan": "Tegas tapi perhatian, bikin kami jadi lebih disiplin",  
+                "pesan":"Semoga semangat dan kepemimpinan kakak terus menginspirasi !!!"# 1
+            },
+            {
+                "nama": "Johannes Krisjon Sitilonga",
+                "nim": "122450043",
+                "umur": "20",
+                "asal":"Tanggerang",
+                "alamat": "Jl. Lapas Raya",
+                "hobbi": "baca buku (dasar-dasar sql)",
+                "sosmed": "@johanneskrisjnnn",s
+                "kesan": "Bijak dan punya pemikiran yang luas",  
+                "pesan":"semangat terus kuliahnya bang !!!"# 1
+            },
+            {
+                "nama": "Elisabeth Claudia Simanjuntak",
+                "nim": "122450123",
+                "umur": "19",
+                "asal":"Tanggerang",
+                "alamat": "Airest Kost",
+                "hobbi": "siram shopee",
+                "sosmed": "@celisabethh_",
+                "kesan": "Kakaknya lembut dan sabar banget waktu menjelaskan ke kami",  
+                "pesan":"Terima kasih sudah jadi panutan, semoga karier kakak lancar selalu !!!"# 1
+            },
+            {
+
+                "nama": "Syadza Puspandari Azhar",
+                "nim": "122450072",
+                "umur": "19",
+                "asal":"Palembang",
+                "alamat": "Belwis",
+                "hobbi": "Membaca",
+                "sosmed": "@celisabethh_",
+                "kesan": "Kakaknya lembut dan sabar banget waktu menjelaskan ke kami",  
+                "pesan":"Terima kasih sudah jadi panutan, semoga karier kakak lancar selalu !!!"# 1
+            },
+            {
+                "nama": "Eksanty F. Sukma Islamiaty",
+                "nim": "122450001",
+                "umur": "19",
+                "asal":"Rote, NTT",
+                "alamat": "rajabasa",
+                "hobbi": "baca buku , saku pramuka",
+                "sosmed": "@ekshantyfebriana",
+                "kesan": "Punya energi positif yang bikin semangat",  
+                "pesan":"Semoga semua hal baik terus menyertai langkah kakak !!!"# 1
+            },
+            {
+                "nama": "Farahanum Afifah Ardiansyah",
+                "nim": "122450056",
+                "umur": "21",
+                "asal":"kota Padang,Sumbar",
+                "alamat": "sukarame",
+                "hobbi": "tidur",
+                "sosmed": "@farahanumafifahh",
+                "kesan": "Seru banget bisa kenal kakak yang suportif dan penuh semangat!",  
+                "pesan":"Semoga semua impian kakak tercapai, dan jangan lupa mampir kasih semangat lagi ke kami !!!"# 1
+            },
+        ]
+        display_images_with_data(gambar_urls, data_list)
+    kesekjenan()
+
+# Tambahkan menu lainnya sesuai kebutuhan
