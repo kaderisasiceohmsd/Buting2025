bc511235584388faa14392e638ded01d80c9653b
jayasaputratamba <jayatamba17@gmail.com>
2025-10-12 22:20:25 +0700

    Update main.py

--- template_code/main.py
+++ template_code/main.py
@@ -43,6 +43,16 @@ Mahasiswa7 = st.Page(
     title="026 - Farhanah Hadaya Fatin",
     icon=":material/person:",
 )
+Mahasiswa8 = st.Page(
+    "Buku Kating/026_Farhanah Hadaya Fatin.py",
+    title="026 - Farhanah Hadaya Fatin",
+    icon=":material/person:",
+)
+Mahasiswa9 = st.Page(
+    "Buku Kating/026_Farhanah Hadaya Fatin.py",
+    title="026 - Farhanah Hadaya Fatin",
+    icon=":material/person:",
+)
 
 #Perlu diperhatikan perubahannya
 KREASI = st.Page("tools/KREASI.py", title="KREASI", icon=":material/search:")
