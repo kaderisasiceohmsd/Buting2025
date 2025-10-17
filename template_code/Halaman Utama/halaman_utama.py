39a0745e85d60945138424d6dcc62501e9627b06
Azzelya Thianandry <azzelya.124450041@student.itera.ac.id>
2025-10-17 22:46:38 +0700

    Change header tags from h2/h1 to h3/h1

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -97,7 +97,7 @@ if menu == "Home":
             unsafe_allow_html=True,
         )
         st.markdown(
-            "<h2 class='centered-title'>BAYESSIAN</h2>", unsafe_allow_html=True
+            "<h1 class='centered-title'>BAYESSIAN</h1>", unsafe_allow_html=True
         )
         st.markdown(
             """<div style="text-align: center;">Think With Logic, Act with data, Move for real impact!!</div>""",
@@ -107,7 +107,7 @@ if menu == "Home":
         foto_kelompok = "https://drive.google.com/uc?export=view&id=1UkHLxu0dW8Yq9bQXVTjhrdIHyGQWZKP4"
         layout(foto_kelompok)
         st.markdown(
-            "<h1 class='centered-title'>Deskripsi Kelompok</h1>", unsafe_allow_html=True
+            "<h3 class='centered-title'>Deskripsi Kelompok</h3>", unsafe_allow_html=True
         )
         st.markdown(
             """<div style="text-align: justify;">
