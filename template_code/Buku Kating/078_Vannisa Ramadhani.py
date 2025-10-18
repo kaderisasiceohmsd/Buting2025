5cfb985ba46ebc2d8d558d37980150db76c70925
Vannisa Ramadhani <vnnsrmdhn@gmail.com>
2025-10-18 22:52:40 +0700

    Update 'TES VANNI kesan BG RENDRA' field for Vannisa Ramadhani

--- template_code/Buku Kating/078_Vannisa Ramadhani.py
+++ template_code/Buku Kating/078_Vannisa Ramadhani.py
@@ -115,7 +115,7 @@ if menu == "Kesekjenan":
                 "alamat": "Pulau Damar",
                 "hobbi": "Menyanyi",
                 "sosmed": "@_erendraa",
-                "kesan": "Sangat berwibawa dan keren",  
+                "kesan": "TES VANNI",  
                 "pesan":"Semangat terus bang menjalani hidup sebagai ketua"# 1
             },
             {
