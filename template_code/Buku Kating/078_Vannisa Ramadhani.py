95e7a9f2855b70d9959d3715489e988c0658d93f
Vannisa Ramadhani <vnnsrmdhn@gmail.com>
2025-10-22 20:25:18 +0700

    update vannisa tes 2

--- template_code/Buku Kating/078_Vannisa Ramadhani.py
+++ template_code/Buku Kating/078_Vannisa Ramadhani.py
@@ -115,7 +115,7 @@ if menu == "Kesekjenan":
                 "alamat": "Pulau Damar",
                 "hobbi": "Menyanyi",
                 "sosmed": "@_erendraa",
-                "kesan": "TES VANNI",  
+                "kesan": "TES VANNI 2",  
                 "pesan":"Semangat terus bang menjalani hidup sebagai ketua"# 1
             },
             {
