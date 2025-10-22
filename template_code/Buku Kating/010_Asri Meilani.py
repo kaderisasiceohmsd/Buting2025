cefd4bed17731658da931438d42d69978c467ebe
asrmeilani <asri.124450010@student.itera.ac.id>
2025-10-23 01:01:02 +0700

    harusnya bisa

--- template_code/Buku Kating/010_Asri Meilani.py
+++ template_code/Buku Kating/010_Asri Meilani.py
@@ -403,7 +403,7 @@ if menu == "Senator":
                 "alamat": "Sukarame",
                 "hobbi": "Denger lagu, maen roblox",
                 "sosmed": "@nadyaanjanani",
-                "kesan": "Kakak ini asik, baik",  
+                "kesan": "kakaknyaa cantik bangettt plisss",  
                 "pesan":"semangat terus kuliahnya, dan langgeng terus ya kak ;)"# 1
             },
             {
