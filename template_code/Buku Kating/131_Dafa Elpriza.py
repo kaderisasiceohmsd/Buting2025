621ee95834ae03d7db79b3445f53e6673c197609
dafaelpriza <elprizadafa@gmail.com>
2025-10-23 17:38:47 +0700

    tes aja

--- template_code/Buku Kating/131_Dafa Elpriza.py
+++ template_code/Buku Kating/131_Dafa Elpriza.py
@@ -115,7 +115,7 @@ if menu == "Kesekjenan":
                 "alamat": "Pulau Damar",
                 "hobbi": "Menyanyi",
                 "sosmed": "@_erendraa",
-                "kesan": "Sangat berwibawa dan keren",  
+                "kesan": "Keren dan kece banget",  
                 "pesan":"Semangat terus bang menjalani hidup sebagai ketua"# 1
             },
             {
