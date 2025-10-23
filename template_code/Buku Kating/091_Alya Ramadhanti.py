df074e22ff2b4bf7a1f8c35577147808f47cdd09
alyaramadhanti <alyaramadhanti430@gmail.com>
2025-10-23 23:30:40 +0700

    up foto

--- template_code/Buku Kating/091_Alya Ramadhanti.py
+++ template_code/Buku Kating/091_Alya Ramadhanti.py
@@ -1013,7 +1013,7 @@ if menu == "Departemen Mikfes":
 if menu == "Departemen Eksternal":
     def Departemen_Eksternal():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1xuRbH7RBb-WesJrYAyWks77SGowD8kSm",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
