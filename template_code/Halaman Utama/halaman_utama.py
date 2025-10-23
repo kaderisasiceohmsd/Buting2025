630161d5a88cac1042a21ad64c07e238535396e6
helmy033 <helmypratama1245@gmail.com>
2025-10-23 22:56:12 +0000

    About Us

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -144,7 +144,7 @@ elif menu == "About Us":
             "https://drive.google.com/uc?export=view&id=1wwBHyFgyIi0idBbpFwosCgw5TXvv2glv", #raisya
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_", #jaya
             "https://drive.google.com/uc?export=view&id=1hAmwIuWyGLNfiwBNUwKsh6Ke-_ihyKbo", #riska
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_", #helmay           
+            "https://drive.google.com/uc?export=view&id=1jn4cNQQAOuWjlsSgFvzPsT1kCze5CklB", #helmay           
         ]
         data_list = [
             {
@@ -214,8 +214,8 @@ elif menu == "About Us":
                 "nama": "Helmy Surya Pratama",
                 "sebagai": "Anggotaa",
                 "nim": "12450033",
-                "fun_fact": "",
-                "motto_hidup": " ",
+                "fun_fact": "Saya Takut Balon",
+                "motto_hidup": "Love the life live, Live the life you love ",
             },
         ]
         display_images_with_data(gambar_urls, data_list)
