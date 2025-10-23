939ee803ee28ca344106e60203a4bc7c62bedb4c
zannubaarifahilman <zannubaariffahilman@gmail.com>
2025-10-23 09:11:17 +0700

    Update 119_Yulia Kristine Malau.py

--- template_code/Buku Kating/119_Yulia Kristine Malau.py
+++ template_code/Buku Kating/119_Yulia Kristine Malau.py
@@ -181,20 +181,20 @@ if menu == "Kesekjenan":
 elif menu == "Baleg":
     def Baleg():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1qhqxoUyqFCnmn5MjrOga9CWmrxlg2pa1",
-            "https://drive.google.com/uc?export=view&id=1nhG44Yf9LG6VjhLDdbx_lIvF4a1AvVOB",
-            "https://drive.google.com/uc?export=view&id=17kz08pPVu15mOGiivNKmfNjhcE8GmjN1",
-            "https://drive.google.com/uc?export=view&id=1v5UrXOuduIvxBnl3c1vAVBxLQvcdKEuk",
-            "https://drive.google.com/uc?export=view&id=1Kh0s6LTRqOtIi7CWI7gsrn6gcu4SD6EJ",
-            "https://drive.google.com/uc?export=view&id=12jJeWep97P9yYbH8AyW1S_dAPMOt6DJx",
-            "https://drive.google.com/uc?export=view&id=1nUm3APcMLftKwS6gX3IcALPoTHMbEXdS",
-            "https://drive.google.com/uc?export=view&id=1H7Wqg0mx5oOjdSMmYcyzpp11FhegUyVX",
-            "https://drive.google.com/uc?export=view&id=1wMaJ6_UZ705wPHCLJe6iVLhFccaWP7NH",
-            "https://drive.google.com/uc?export=view&id=15OexmIE_UaVggDMXxUHUlYRv0L3Ekzl1",
-            "https://drive.google.com/uc?export=view&id=13z9ggaL_OUgevBYYtwtAzULY6pJe53pj",
-            "https://drive.google.com/uc?export=view&id=17xWlh1Knxhqpjk2LapYj_2lQ9esbZiAR",
-            "https://drive.google.com/uc?export=view&id=1XszkR-kyXo8qHaCZP3gi04UkOmGqSVbZ",
-            "https://drive.google.com/uc?export=view&id=1Tqd46sxxISB17qcpIbybUJwJaVQuJplA",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
         ]
         data_list = [
             {
@@ -358,10 +358,10 @@ elif menu == "Baleg":
 elif menu == "Senator":
     def Senator():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1n7-mkDuBFJXIDyq-ZX4FFbLOgj1CmM3n",
-            "https://drive.google.com/uc?export=view&id=1xFIgU9ZvNb5qQJt3ebzjQWF14wyZFRAU",
-            "https://drive.google.com/uc?export=view&id=1LN4Hvl-Mj5LetNQvMPgMhS1fpQtfYB4c",
-            "https://drive.google.com/uc?export=view&id=1IK3Uelukq9rie1KAh4W4aueypgBdJvAe",  
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",
+            "https://drive.google.com/uc?export=view&id=1",  
         ]   
         data_list = [
             {
@@ -411,5 +411,6 @@ elif menu == "Senator":
         ]
         display_images_with_data(gambar_urls, data_list)
     Senator()
+
 
 
