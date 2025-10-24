2fe5fda617820e7ebf5e1d7b46e3c05accf94509
Andra Ilham Bintang <andrailhambintang06@gmail.com>
2025-10-24 10:03:55 +0700

    Fix duplicate Google Drive link in the code
    Removed duplicate Google Drive link from the list.

--- template_code/Buku Kating/067_Ghiyats Thabularasa Meardhy.py
+++ template_code/Buku Kating/067_Ghiyats Thabularasa Meardhy.py
@@ -747,7 +747,7 @@ if menu == "Departemen Mikfes":
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google  .com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
