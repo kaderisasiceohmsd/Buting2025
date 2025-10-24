5d05c7b2f52fea280b284d0a8c3dd421a6d33d14
alyaramadhanti <alyaramadhanti430@gmail.com>
2025-10-24 23:29:39 +0700

    alya done

--- template_code/Buku Kating/091_Alya Ramadhanti.py
+++ template_code/Buku Kating/091_Alya Ramadhanti.py
@@ -433,7 +433,7 @@ if menu == "Departemen PSDA":
             "https://drive.google.com/uc?export=view&id=1IOG0kRkFxvaMx2DBA56TOL9QWsDyiwYP",
             "https://drive.google.com/uc?export=view&id=1rcDxsPPJihMooXck_rklsoXjhT7iIhRz",
             "https://drive.google.com/uc?export=view&id=1_e8zUF3qwfVUSKSbTAuMUL3fClCGB3g-",
-            "https://drive.google.com/uc?export=view&id=1Xid8SlT_jmzzNh6ghiH-RUyvURfq6hIX",
+            "https://drive.google.com/uc?export=view&id=1RlOnWmA2TW8c-9VyOMjnG-QnnEOrX20b",
             "https://drive.google.com/uc?export=view&id=1Xid8SlT_jmzzNh6ghiH-RUyvURfq6hIX",
             "https://drive.google.com/uc?export=view&id=1i9iLZxd7AdwICLutme7rM4QXyEicY7EG",
             "https://drive.google.com/uc?export=view&id=1IfktzCVAf0peghC_fNWtGkz1YTMmNLrJ",
