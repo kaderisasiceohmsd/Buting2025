ebafd1e81188558319391d97711e6185fc423243
syafiq-sss <71967485+syafiq-sss@users.noreply.github.com>
2025-10-24 23:20:09 +0700

    Update data_list with new user information

--- template_code/Buku Kating/099_Muhammad Syafiqul Falakh.py
+++ template_code/Buku Kating/099_Muhammad Syafiqul Falakh.py
@@ -450,12 +450,12 @@ if menu == "Departemen PSDA":
         data_list = [
             {
                 "nama": "Ferdy Kevin Naibaho",
-                "nim": "",
-                "umur": "",
-                "asal": "",
-                "alamat": "",
-                "hobbi": "",
-                "sosmed": "",
+                "nim": "12250107",
+                "umur": "21",
+                "asal": "Medan",
+                "alamat": "Tanjung Senang",
+                "hobbi": "Futsal",
+                "sosmed": "@ferdy_kevin",
                 "kesan": "Terima kasih banyak atas ilmunya, Kak.",
                 "pesan": "Sukses selalu ya, Kak!"
             },
