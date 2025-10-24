23ae60604ac0644262adcac4f7a9a679564e8efe
Andra Ilham Bintang <andrailhambintang06@gmail.com>
2025-10-24 09:45:53 +0700

    andra

--- template_code/Buku Kating/060_Andra Ilham Bintang.py
+++ template_code/Buku Kating/060_Andra Ilham Bintang.py
@@ -1499,18 +1499,17 @@ if menu == "Departemen Internal":
 if menu == "Departemen SSD":
     def Departemen_SSD():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1MEcPKZPIhzA_bpVYgJCGyehid0OsBNjq",
+            "https://drive.google.com/uc?export=view&id=1fIiWNdiseRk8igFc8GJK7KGlCxx96zbC",
+            "https://drive.google.com/uc?export=view&id=1XkhSTnYSJ3BF8fZ-uR0fS6rGN9crwrbT",
+            "https://drive.google.com/uc?export=view&id=1y4nDEp2AL-0G0NBYL628vKdfAz_xrQJJ",
+            "https://drive.google.com/uc?export=view&id=1WOp3XSj3r-MqBa46lT28ro_ic9ZN7p29",
+            "https://drive.google.com/uc?export=view&id=1Q06yo9PAPYQLtjsihr8evAYDNadFpw-W",
+            "https://drive.google.com/uc?export=view&id=144p3IQ-IPLCSQcI12Tiv9RiYCrWCgDTh",
+            "https://drive.google.com/uc?export=view&id=1yqrZniGqeRmKyGw_pQQ60pNDBAQuxRU_",
+            "https://drive.google.com/uc?export=view&id=1TgHZHSwtjx48lwnPv8EAUOO8anGx8BXQ",
+            "https://drive.google.com/uc?export=view&id=14HsoKczUMDILfbUZfENvX_H0O3_fVANk",
+            "https://drive.google.com/uc?export=view&id=1HqOjcaHuOQXBvEFomA0Jssu4vyps0k2L",
         ]
         data_list = [
             {
