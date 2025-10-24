feef29c0f69e2d010f45ee30422daf0851c7603e
bimayasa <bima.124450106@student.itera.ac.id>
2025-10-24 15:40:30 +0700

    lagi

--- template_code/Halaman Utama/halaman_utama.py
+++ template_code/Halaman Utama/halaman_utama.py
@@ -16,7 +16,7 @@ st.markdown(
 
         /* ======= Sidebar gradasi ======= */
         [data-testid="stSidebar"] {
-            background: linear-gradient(to bottom, #F5EFE6, #E8DFCA, #CBDCEB);
+            background: linear-gradient(to bottom, #ABE7B2, #CBF3BB, #ECF4E8);
             color: #D7C6BB;
         }
 
